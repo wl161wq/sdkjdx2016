2867070df8daebe84c6aed1ddcb93eacf8760f19
nlw <1353905492@2qq.com>
2019-05-21 17:04:27 +0800

    new

--- test/src/main/java/com.isoft.dao/impl/UserDAOImpl.java
+++ test/src/main/java/com.isoft.dao/impl/UserDAOImpl.java
@@ -1,15 +1,35 @@
 package com.isoft.dao.impl;
 
 import com.isoft.dao.IUserDAO;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository("userDAO")
 public class UserDAOImpl implements IUserDAO {
+    @Autowired
+    SqlSessionFactory sessionFactoryBean;
     @Override
     public String login(String uname, String upwd) {
-        System.out.println(uname+","+upwd);
-        return "success";
+        SqlSession sqlSession=sessionFactoryBean.openSession(true);
+        String statment="com.isoft.mapping.userMapper.login";
+        Map map=new HashMap();
+        map.put("uname",uname);
+        map.put("upwd",upwd);
+        List<Map<String,Object>> list=sqlSession.selectList(statment,map);
+        if(list.size()>0){
+            return "success";
+        }
+        else {
+            return "fault";
+        }
     }
 
     @Override
